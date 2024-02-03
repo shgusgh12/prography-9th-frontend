931ced36cca7e976108035ccdd262357ddb23dc2
shgusgh12 <https://github.com/shgusgh12>
2024-02-04 08:09:40 +0900

    Add: 디자인 추가

--- src/components/main/filter/Filter.js
+++ src/components/main/filter/Filter.js
@@ -8,6 +8,23 @@ export const Filter = ({
   handleImgChange,
   handleSortChange,
 }) => {
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      borderColor: state.isFocused ? '#ff7f7f' : provided.borderColor,
+      boxShadow: state.isFocused ? '0 0 0 1px #ff7f7f' : null,
+      '&:hover': {
+        borderColor: '#ff7f7f',
+      },
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? '#ff7f7f' : 'white',
+      '&:hover': {
+        backgroundColor: '#ff7f7f',
+      },
+    }),
+  };
   return (
     <div className={styles.filter_container}>
       <p>
@@ -16,12 +33,14 @@ export const Filter = ({
 
       <div className={styles.select_container}>
         <Select
+          styles={customStyles}
           className={styles.select}
           options={SortFilter}
           placeholder='정렬 방법을 선택하세요'
           onChange={handleSortChange}
         ></Select>
         <Select
+          styles={customStyles}
           className={styles.select}
           options={ImgFilter}
           placeholder='이미지 개수를 선택하세요'
